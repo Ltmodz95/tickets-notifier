938a15cdd5db1be1d6ab4a760e4209895e6beb7a
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2024-09-07 19:10:54 +0200

    Adding test to user creation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    send_due_date_reminder { false }
+    due_date_reminder_interval { 1 }
+    due_date_reminder_time { Time.current.in_time_zone(time_zone).strftime('%H:%M') }
+    time_zone { 'UTC' }
+
+    trait :with_invalid_name do
+      name { '' }
+    end
+    trait :with_invalid_email do
+      email { 'bademail.com' }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  let(:bad_email_user) { build(:user, :with_invalid_email) }
+  let(:bad_name_user) { build(:user, :with_invalid_name) }
+
+  describe '.create' do
+    context 'when creating user with valid data' do
+      it { expect(user.valid?).to be true }
+    end
+
+    context 'when creating user invalid data' do
+      it do
+        bad_email_user.validate
+        expect(bad_email_user.errors.full_messages.first).to eq 'Email is invalid'
+      end
+
+      it do
+        bad_name_user.validate
+        expect(bad_name_user.errors.full_messages.first).to eq "Name can't be blank"
+      end
+    end
+  end
 end
